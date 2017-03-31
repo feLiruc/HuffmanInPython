2161c7eee8cf5a80969d680286b54c7f06a7b1ef
feLiruc <eu@felipemarinho.com.br>
2017-03-31 17:59:48 +0000

    Criado classe de nós pra implementação da heap

---
+++ Encode.py
@@ -0,0 +1,29 @@
+from itertools import groupby
+from heapq import *
+
+# Node para abstração dos nós da heap que vai ser gerada
+class Node(object):
+    esquerda = None
+    direita = None
+    dado = None
+    peso = 0
+
+    # Metódo construtor pro object Node iniciando dado e peso de cada nó
+    def __init__(self, d, p):
+        self.dado = d
+        self.peso = p
+
+    # Método para setar os nós filhos(esquerdo e direito) de cada nó da pilha
+    def setChildren(self, ne, nd):
+        self.esquerda = ne
+        self.direita = nd
+
+    # Método sobreposto pra visualização das informações(print) dentro do nó
+    def __repr__(self):
+        return "Dado: "+str(self.dado)+" - Peso: "+str(self.peso)+" - esquerda: "+str(self.esquerda)+" - direita: "+str(self.direita)+"\n"
+    
+    def __cmp__(self, other):
+        return cmp(self.peso, other.peso)
+
+    def __lt__(self, other):
+        return self.peso < other.peso
